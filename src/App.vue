5b9101cd966afb23df665c05749a1ef7b744589a
jdano <julian.dano2016@gmail.com>
2018-11-22 22:26:19 -0500

    added home hero section

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,82 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to your Vue.js App"/> -->
+    <home-hero />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+// import HelloWorld from './components/HelloWorld.vue';
+import HomeHero from './components/HomeHero.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    HomeHero,
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+html, body {
+  overflow-x: hidden;
 }
+
+body {
+  margin: 0px !important;
+}
+
+.theme-color-green {
+  color: #50ada2;
+}
+
+.theme-color-white {
+  color: #FCFAF9;
+}
+
+.theme-inline-button {
+  display: inline-block;
+  padding: 20px;
+  text-decoration: none;
+  border-radius: 5px;
+  border: 3px solid #50ada2;
+  font-size: 14px;
+  transition: all 0.3s ease-out;
+
+  &:hover {
+    background-color: #50ada2;
+    color: #FCFAF9;
+  }
+  &:focus {
+    outline: 0;
+  }
+}
+
+.theme-inline-button-primary {
+  background-color: #50ada2;
+  color: #FCFAF9;
+}
+
+.theme-inline-button-secondary {
+  background-color: transparent;
+  color: #50ada2;
+}
+
+h1, h2, h3, h4, h5, p, a {
+  font-family: 'Josefin Sans', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  text-rendering: optimizeLegibility;
+}
+
+h1 {
+  font-size: calc(46px + (80 - 46) * ((100vw - 300px) / (1600 - 300)));
+  margin: 0;
+}
+
+p, a {
+  font-size: calc(14px + (24 - 14) * ((100vw - 300px) / (1600 - 300)));
+}
+
 </style>
---
+++ src/components/HomeHero.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="home-hero">
+        <div class="hh-background-img"></div>
+        <section class="hh-content theme-color-white">
+            <h1 class="hh-title"><span class="theme-color-green">told</span>.design</h1>
+            <p class="hh-subtitle">website and content development for mission-driven startups</p>
+            <div class="hh-button-group">
+                <a class="theme-inline-button theme-inline-button-primary" href="#">WORK WITH US</a>
+                <a class="theme-inline-button theme-inline-button-secondary" href="#">VIEW PORTFOLIO</a>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "HomeHero",
+};
+</script>
+
+<style lang="scss" scoped>
+
+.home-hero {
+    height: 100vh;
+    width: 100%;
+    position: relative;
+
+    @media screen and (min-width: 768px) {
+        height: 90vh;
+    }
+}
+
+.hh-background-img {
+    height: 100%;
+    background: url("../assets/iphone-desk-laptop-notebook.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    width: 100%;
+    filter: grayscale(100%) brightness(50%);
+}
+
+.hh-content {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    width: 100%;
+    max-width: calc(100vw - 50px);
+}
+
+.hh-title {
+    margin-bottom: 50px;
+}
+
+.hh-subtitle {
+    font-size: calc(18px + (28 - 18) * ((100vw - 300px) / (1600 - 300)));
+    margin-bottom: 50px;
+}
+
+.hh-button-group > a {
+
+    @media screen and (max-width: 450px) {
+        display: block;
+        border-radius: 0px;
+    }
+
+    &:first-child {
+        @media screen and (min-width: 450px) {
+            margin-right: 25px;
+        }
+    }
+}
+
+</style>
