604c81950702c819d933063cc7a40101ec31a0b2
jdano <julian.dano2016@gmail.com>
2018-11-26 09:09:39 -0500

    started Contact section

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
       <OurTeam />
       <TestimonialOverview />
       <Testimonials />
+      <Contact />
     </div>
   </div>
 </template>
@@ -18,6 +19,7 @@ import TeamOverview from './components/TeamOverview.vue';
 import OurTeam from './components/OurTeam.vue';
 import TestimonialOverview from './components/TestimonialOverview.vue';
 import Testimonials from './components/Testimonials.vue';
+import Contact from './components/Contact.vue';
 
 export default {
   name: 'app',
@@ -28,6 +30,7 @@ export default {
     OurTeam,
     TestimonialOverview,
     Testimonials,
+    Contact,
   },
 };
 </script>
---
+++ src/components/Contact.vue
@@ -0,0 +1,48 @@
+<template>
+    <section class="contact">
+        <div class="contact-back-filter"></div>
+        <div class="contact-grid">
+            <h3>Get in touch.</h3>
+            <p>Questions? Want a quote? Just want to say, "What's Up?" Use the contact form and we will get back to you the same day.</p>
+            <p>Or email us directly at <a href="#">hello@told.design</a>.</p>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Contact',
+}
+</script>
+
+<style lang="scss" scoped>
+
+.contact {
+    background: url("../assets/open-sign.png");
+    background-repeat: no-repeat;
+    background-size: cover;
+    padding: 50px;
+    position: relative;
+}
+
+.contact-back-filter {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    background-color: rgba(0,0,0,.5);
+}
+
+.contact-grid {
+    position: relative;
+    display: grid;
+    grid-template-columns: 1fr;
+
+    h1, h2, h3, h4, h5, p, a {
+        color: white;
+        margin-bottom: 15px;
+    }
+}
+
+</style>
