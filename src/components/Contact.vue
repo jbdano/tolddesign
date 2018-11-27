3deb7381cf3141693455a5aadf48667e254425df
jdano <julian.dano2016@gmail.com>
2018-11-26 23:22:51 -0500

    added initial framework for Contact Form section

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -2,16 +2,26 @@
     <section class="contact">
         <div class="contact-back-filter"></div>
         <div class="contact-grid">
-            <h3>Get in touch.</h3>
-            <p>Questions? Want a quote? Just want to say, "What's Up?" Use the contact form and we will get back to you the same day.</p>
-            <p>Or email us directly at <a href="#">hello@told.design</a>.</p>
+            <div class="contact-grid-content">
+                <h3 class="contact-content-title">Get in touch.</h3>
+                <p>Questions? Want a quote? Just want to say, "What's Up?" Use the contact form and we will get back to you the same day.</p>
+                <p>Or email us directly at <a href="#">hello@told.design</a>.</p>
+                <button class="theme-inline-button theme-inline-button-primary contact-email-btn">Email Us</button>
+            </div>
+            <div class="contact-grid-form">
+                <ContactForm />
+            </div>
         </div>
     </section>
 </template>
 
 <script>
+import ContactForm from './ContactForm.vue';
 export default {
     name: 'Contact',
+      components: {
+        ContactForm,
+    },
 }
 </script>
 
@@ -21,8 +31,12 @@ export default {
     background: url("../assets/open-sign.png");
     background-repeat: no-repeat;
     background-size: cover;
-    padding: 50px;
+    padding: 75px 20px;
     position: relative;
+
+    @media screen and (min-width: 992px) {
+        padding: 125px 20px;
+    }
 }
 
 .contact-back-filter {
@@ -38,11 +52,32 @@ export default {
     position: relative;
     display: grid;
     grid-template-columns: 1fr;
+    grid-row-gap: 125px;
+    width: 1000px;
+    max-width: 100%;
+    margin: 0 auto;
+
+    @media screen and (min-width: 992px) {
+        grid-template-columns: 1fr 1fr;
+        grid-column-gap: 100px;
+    }
 
     h1, h2, h3, h4, h5, p, a {
         color: white;
         margin-bottom: 15px;
     }
+}
+
+.contact-grid-content {
+    padding: 0px 15px;
+}
+
+.contact-content-title {
+    font-weight: bold;
+}
+
+.contact-email-btn {
+    padding: 15px 30px;
 }
 
 </style>
---
+++ src/components/ContactForm.vue
@@ -0,0 +1,111 @@
+<template>
+    <div class="contact-form">
+        <div class="cf-main">
+            <div class="cf-banner">
+                <p class="cf-banner-title">Contact Us</p>
+            </div>
+            <form class="cf-form">
+                <div class="form-group">
+                    <input type="text" class="form-control cf-input" value="" placeholder="Your Name">
+                </div>
+                <div class="form-group">
+                    <input type="email" class="form-control cf-input" value="" placeholder="Your Email">
+                </div>
+                <div class="form-group">
+                    <input type="text" class="form-control cf-input" value="" placeholder="Subject">
+                </div>
+                <div class="form-group">
+                    <textarea value="" class="form-control cf-input cf-input-message" placeholder="Tell us what you're looking for" rows="8"></textarea>
+                </div>
+                <div class="cf-footer">
+                    <button type="submit" class="theme-inline-button theme-inline-button-primary cf-submit-btn">SEND IT</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+import jQuery from 'jquery';
+
+window.jQuery = jQuery;
+window.$ = jQuery;
+
+require('bootstrap');
+
+export default {
+    name: 'ContactForm',
+}
+</script>
+
+<style lang="scss" scoped>
+
+.contact-form {
+    max-width: 425px;
+    margin: 0 auto;
+}
+
+.cf-main {
+    background-color: #FCFAF9;
+    padding: 75px 15px 25px;
+    border-radius: 5px;
+    position: relative;
+
+    @media screen and (min-width: 992px) {
+        padding: 75px 40px 25px;
+    }
+}
+
+.cf-input {
+    background-color: transparent;
+    border-radius: 0px;
+    border-top: none;
+    border-right: none;
+    border-left: none;
+    color: #3C4858;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.25);
+    box-shadow: none;
+    -webkit-box-shadow: none;
+
+    &:focus {
+        border-bottom: 2px solid #50ada2;
+    }
+    &::placeholder {
+        color: rgba(0, 0, 0, 0.25);
+        font-family: 'Josefin Sans', sans-serif;
+    }
+    &-message {
+        resize: none;
+    }
+}
+
+.cf-banner {
+    padding: 25px 0px;
+    width: 80%;
+    background-color: #50ada2;
+    position: absolute;
+    top: 0;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border-radius: 5px;
+    text-align: center;
+    box-shadow: 0 16px 38px -12px rgba(0, 0, 0, 0.56), 0 4px 25px 0 rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2);
+}
+
+.cf-banner-title {
+    color: #FCFAF9;
+    font-weight: bold;
+    margin: 0;
+}
+
+.cf-footer {
+    text-align: right;
+}
+
+.cf-submit-btn {
+    color: #FCFAF9;
+    padding: 15px 30px;
+    margin-top: 20px;
+}
+
+</style>
